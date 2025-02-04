46cfb99afc325744e9b95c0753016f131fde5a4a
grovre <landonvee111@gmail.com>
2025-02-04 12:31:28 -0600

    Swappable repo owner

--- CS2DumperSourceGenerator/CS2DumperSourceGenerator.cs
+++ CS2DumperSourceGenerator/CS2DumperSourceGenerator.cs
@@ -10,57 +10,48 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Net.WebRequestMethods;
 
-namespace A2XDumperSourceGenerator
+namespace CS2DumperSourceGenerator
 {
     [Generator]
-    public class A2XDumperSourceGenerator : ISourceGenerator, IIncrementalGenerator
+    public class A2XDumperSourceGenerator : IIncrementalGenerator
     {
+        const string User = "Grovre";
+
         readonly Dictionary<string, string[]> Links = new()
         {
             {"Schemas", [
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/worldrenderer_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/vphysics2_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/soundsystem_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/server_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/schemasystem_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/scenesystem_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/resourcesystem_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/rendersystemdx11_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/pulse_system_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/particles_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/panorama_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/networksystem_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/materialsystem2_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/host_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/engine2_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/client_dll.cs",
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/animationsystem_dll.cs"
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/worldrenderer_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/vphysics2_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/soundsystem_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/server_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/schemasystem_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/scenesystem_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/resourcesystem_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/rendersystemdx11_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/pulse_system_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/particles_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/panorama_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/networksystem_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/materialsystem2_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/host_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/engine2_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/client_dll.cs",
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/animationsystem_dll.cs"
                 ]
             },
             { "Interfaces", [
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/interfaces.cs"
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/interfaces.cs"
                 ]
             },
             { "Offsets", [
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/offsets.cs"
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/offsets.cs"
                 ]
             },
             { "Buttons", [
-                "https://raw.githubusercontent.com/Grovre/cs2-dumper/refs/heads/main/output/buttons.cs"
+                $"https://raw.githubusercontent.com/{User}/cs2-dumper/refs/heads/main/output/buttons.cs"
                 ]
             }
         };
-
-        public void Execute(GeneratorExecutionContext context)
-        {
-            Debugger.Launch();
-
-        }
-
-        public void Initialize(GeneratorInitializationContext context)
-        {
-            // Do nothing, be happy
-        }
 
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
@@ -76,7 +67,6 @@ namespace A2XDumperSourceGenerator
                 foreach (var (Link, SourceTask) in getTasks)
                 {
                     var source = SourceTask.GetAwaiter().GetResult();
-                    source = "// TEST!!!\n" + source;
                     var hintName = Link.Split('/').Last().Split('.').First();
 
                     context2.AddSource(hintName, source);
